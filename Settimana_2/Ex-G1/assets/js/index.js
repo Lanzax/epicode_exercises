227b701d09da82f338cfa23a35fed96003fb3dd2
Lanzax <118267183+Lanzax@users.noreply.github.com>
2023-02-06 16:14:34 +0100

    esercizio 1 java

---
+++ Settimana_2/Ex-G1/assets/js/index.js
@@ -0,0 +1,7 @@
+
+alert(' primo messaggio');
+alert('secondo messaggio');
+
+document.getElementById('titolo').innerHTML = 'Iniziamo la nostra pratica';  
+document.getElementById('primoParagrafo').innerHTML = 'output scritto da javascript';  
+
---
+++ Settimana_2/Ex-G1/assets/js/variables.js
@@ -0,0 +1,9 @@
+var firstPet = 'cane';
+console.log(firstPet);
+var secondpet2 = 'gatto';
+console.log(secondpet2);
+var third_pet = 'coniglio';
+console.log(third_pet);
+var htmlpet = 'topo';
+document.getElementById('secondoParagrafo').innerHTML = htmlpet +' scritto con javascript';  
+
