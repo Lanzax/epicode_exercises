73a72458a38b1806ccc0c726f79a9dfb7b1e0be1
Lanzax <118267183+Lanzax@users.noreply.github.com>
2023-03-14 17:03:09 +0100

    Primo finito

--- Settimana_7/Ex-G2/assets/js/script.js
+++ Settimana_7/Ex-G2/assets/js/script.js
@@ -1,28 +1,34 @@
-const textArea= document.getElementById('textArea')
-const saveButton= document.getElementById('saveButton')
-const loadButton= document.getElementById('loadButton')
-const resetButton= document.getElementById('resetButton')
+const textArea= document.getElementById('textArea');
+const saveButton= document.getElementById('saveButton');
+const loadButton= document.getElementById('loadButton');
+const resetButton= document.getElementById('resetButton');
 
 const resetFunction= function(){
-    textArea.value=''
+    textArea.value='';
 }
 const saveFunction= function(){
-    const saveLocal =textArea.value
-    localStorage.setItem('formText',saveLocal)
-    console.log('Il testo è stato correttamente salvato!')
+    const saveLocal =textArea.value;
+    localStorage.setItem('formText',saveLocal);
+    crateFunction();
+    console.log('Il testo è stato correttamente salvato!');
 }
 const loadFunction= function(){
-    const loadLocal = localStorage.getItem('formText')
+    const loadLocal = localStorage.getItem('formText');
     if(loadLocal){
-        textArea.value=loadLocal
-        console.log('Il testo è stato correttamente caricato!')
+        textArea.value=loadLocal;
+        console.log('Il testo è stato correttamente caricato!');
         
     }else{
-        console.log('ERRORE NON ESISTE ALCUN FILE!!!!')
+        console.log('ERRORE NON ESISTE ALCUN FILE!!!!');
     }
-
-
 }
-saveButton.onclick=saveFunction
-loadButton.onclick=loadFunction
-resetButton.onclick=resetFunction
+
+const crateFunction= function(){
+    let lista=document.createElement('li');
+    let text=document.createTextNode(textArea.value)
+    lista.appendChild(text);
+    document.getElementById('list').appendChild(lista);
+}
+saveButton.onclick=saveFunction;
+loadButton.onclick=loadFunction;
+resetButton.onclick=resetFunction;
